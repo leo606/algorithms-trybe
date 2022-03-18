57aef7d9a1e4afbe7bf18c58a15396f4aa3f9c47
Leonardo Ferreira <leonardoferreira@protonmail.com>
2022-03-18 10:42:40 -0300

    feat: req 1 - study schedule

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,13 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    if not target_time or target_time < 0:
+        return
+
+    students_in_target_time = 0  # students counter
+    for in_time, out_time in permanence_period:
+
+        if not isinstance(in_time, int) or not isinstance(out_time, int):
+            return
+        # target_time between in_time and out_time
+        elif in_time <= target_time <= out_time:
+            students_in_target_time += 1  # add 1 in students counter
+    return students_in_target_time
