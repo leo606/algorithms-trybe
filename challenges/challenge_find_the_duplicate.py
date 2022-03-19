d5f1c3ac2593f704f1dab91a3d79b8cb03eae648
Leonardo Ferreira <leonardoferreira@protonmail.com>
2022-03-19 10:50:07 -0300

    feat: req 4 - repeated number (find algorithm)

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,2 +1,19 @@
+def search(array, value):
+    for index, element in enumerate(array):
+        if element == value:
+            return index
+    return False
+
+
 def find_duplicate(nums):
-    """ Faça o código aqui. """
+    if not nums:
+        return False
+
+    for index, number in enumerate(nums):
+        if type(number) != int or number < 1:
+            return False
+
+        duplicated_in_array = search(nums, number)
+        if duplicated_in_array != index:
+            return nums[duplicated_in_array]
+    return False
