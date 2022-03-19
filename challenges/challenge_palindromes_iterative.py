5d0108b609e84d995f43f15126153abd67b15b10
Leonardo Ferreira <leonardoferreira@protonmail.com>
2022-03-19 12:27:35 -0300

    feat: req 5 - palindrome iterative super fast

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,13 +1,5 @@
 def is_palindrome_iterative(word):
-    if not word or word[0] != word[-1]:
+    if word:
+        return word == word[::-1]
+    else:
         return False
-
-    for index in range(1, len(word) - 2):
-        if word[index] != word[-(index + 1)]:
-            return False
-    return True
-
-
-word = "agua"
-
-print(is_palindrome_iterative(word))
