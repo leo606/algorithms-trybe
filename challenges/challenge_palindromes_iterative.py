f155a5465ac5d28f960974859dc6535b6a7a5358
Leonardo Ferreira <leonardoferreira@protonmail.com>
2022-03-19 12:24:21 -0300

    feat: palindrome iterative

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,2 +1,13 @@
 def is_palindrome_iterative(word):
-    """ Faça o código aqui. """
+    if not word or word[0] != word[-1]:
+        return False
+
+    for index in range(1, len(word) - 2):
+        if word[index] != word[-(index + 1)]:
+            return False
+    return True
+
+
+word = "agua"
+
+print(is_palindrome_iterative(word))
